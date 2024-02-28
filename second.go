27f2b2de0a776d3e8486f18b6c3e0f5dd7755f08
agberoz <jayakrishnan@onepane.ai>
2024-02-28 16:05:55 +0530

    first commit

---
+++ second.go
@@ -0,0 +1,20 @@
+package main
+
+// find average given function parameters 
+// no return statments allowed
+func average(a, b int){
+
+	average := a+b/2
+	
+}
+
+// find min and maxmimum from the given function parameters
+// 
+func min_max(a, b, c int) (int, int){
+
+	min := 0
+	max := 0
+
+	
+	return min
+}
---
+++ third.go
@@ -0,0 +1,12 @@
+
+package main
+
+
+func calculate_circle_area(r float64)(int){
+	const pi float64 = 5
+
+	pi = 3.14
+
+	return pi*r
+
+}
